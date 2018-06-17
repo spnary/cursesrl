61728d0815ad58dff162163e2f3bc5fa1c9e5135
Stephen Nary <spnary@gmail.com>
2018-06-17 13:06:32 -0400

    add support for vi movement keys and use 'q' to quit

--- object.c
+++ object.c
@@ -10,21 +10,26 @@ void moveObject(Object *obj, Tile map[MAP_WIDTH][MAP_HEIGHT],  int x, int y) {
 
 int handleKey(int key, Object *obj, Tile map[MAP_WIDTH][MAP_HEIGHT]) {
 	switch(key) {
+		case 'k':
 		case KEY_UP:
 			moveObject(obj, map, 0, -1);
 			return 0;
+		case 'j':
 		case KEY_DOWN:
 			moveObject(obj, map, 0, 1);
 			return 0;
+		case 'h':
 		case KEY_LEFT:
 			moveObject(obj, map, -1, 0);
 				return 0;
+		case 'l':
 		case KEY_RIGHT:
 			moveObject(obj, map, 1, 0);
 				return 0;
-
-		default:
+		case 'q':
 				return 1;
+		default:
+				return 0;
 	}
 }
 
